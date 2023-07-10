f940e49f4dda7f169915d0e039ad3cd2bc06b5c1
hakeemellis <114674803+hakeemellis@users.noreply.github.com>
2023-07-10 13:00:06 -0400

    Updated files

--- js/main.js
+++ js/main.js
@@ -1,29 +1,35 @@
 // For Navigation Bar Transparency // 
+window.addEventListener('scroll', function() {
+  var navbar = document.querySelector('.navbar');
+  var heroSection = document.querySelector('.hero');
+  
+  console.log('Page Y Offset:', window.pageYOffset);
+  console.log('Hero Section Offset Top:', heroSection.offsetTop);
+  
+  if (window.pageYOffset > heroSection.offsetTop) {
+    navbar.classList.add('bg-black');
+    console.log('Navbar scrolled');
+  } else {
+    navbar.classList.remove('bg-black');
+    console.log('Navbar not scrolled');
+  }
+});
 
-  window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar');
-    var heroSection = document.querySelector('.hero-section');
-    
-    if (window.scrollY > heroSection.offsetHeight) {
-      navbar.classList.add('bg-black');
-    } else {
-      navbar.classList.remove('bg-black');
-    }
-  });
 
-// For Nav Bar changing color when click in Mobile //
-
+// For Nav Bar changing color when clicked in Mobile //
 var navbar = document.querySelector('.navbar-expand-lg');
 var navbarCollapse = document.querySelector('.navbar-collapse');
 
 navbarCollapse.addEventListener('show.bs.collapse', function() {
   navbar.classList.add('bg-black');
-  navbar.style.marginTop = '0'; 
+  navbar.style.marginTop = '0';
+  console.log('Navbar collapse shown');
 });
 
 navbarCollapse.addEventListener('hide.bs.collapse', function() {
   if (!navbar.classList.contains('show')) {
     navbar.classList.remove('bg-black');
-    navbar.style.marginTop = '0'; 
+    navbar.style.marginTop = '0';
+    console.log('Navbar collapse hidden');
   }
 });
